c5827c7fd370caef2488040f5b1be33f5b7deec4
Ryan Lee <ryanm.lee11@gmail.com>
2020-12-28 21:27:42 -0500

    Product update

--- components/homepage/HeroSection.js
+++ components/homepage/HeroSection.js
@@ -38,7 +38,7 @@ export default class HeroSection extends Component {
                 }}
               >
                 <p className="font-size-display5 font-family-secondary mb-4 text-center hero-header">
-                  Welcome to the world of scotch whisky!
+                  Welcome to the world of scotch whiskey!
                 </p>
                 <p className="text-transform-uppercase font-size-title mb-5 hero-subheader">
                   A range of products for you
